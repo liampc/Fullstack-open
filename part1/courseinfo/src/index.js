a4c8d03953af6b324f4366bd3ff4d2a029f2320f
Kathlia Carlos <lia.carlos23@gmail.com>
2020-08-04 11:08:01 +0800

    step4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -43,23 +43,25 @@ const Total = (props) => {
 const App = () => {
 
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [
+    {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+    },
+    {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+    },
+    {
     name: 'State of a component',
     exercises: 14
-  }
+    }
+  ]
   return (
     <div>
       <Header name={course} />
-      <Content part={[part1.name,part2.name, part3.name]} number={[part1.exercises,part2.exercises,part3.exercises]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content part={parts.map(n => n.name)} number={parts.map(n => n.exercises)} />
+      <Total total={parts.map(n => n.exercises)} />
     </div>
     
   )
