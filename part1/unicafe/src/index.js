20652b3a3ed1a81330eb34f4cbe1c11e59dd96c3
Kathlia Carlos <lia.carlos23@gmail.com>
2020-08-12 16:00:01 +0800

    step1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Button = ({score, text}) => {
+  return (
+     <button onClick={score}> {text} </button>
+  )
+}
+  
+const Numbers = ({text, num}) => {
+  return (
+  <>
+    <p> {text} : {num}</p>
+  </>
+  )
+}
+ 
+
+
+
+const App = () => {
+  const [score, setScore] = useState({
+    good: 0,
+    bad: 0,
+    neutral: 0
+  })
+
+  const setGood = () => {
+    setScore({
+      ...score,
+      good: score.good + 1
+    })
+  }
+
+  const setBad = () => {
+    setScore({
+      ...score,
+      bad: score.bad + 1
+    })
+  }
+
+  const setNeutral = () => {
+    setScore({
+      ...score,
+      neutral: score.neutral + 1
+    })
+  }
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button score={setGood} text={'Good'}/>
+      <Button score={setBad} text={'Bad'}/>
+      <Button score={setNeutral} text={'Neutral'}/>
+      <h2> Statistics</h2>
+      <Numbers text={'Good'} num={score.good} />
+      <Numbers text={'Bad'} num={score.bad} />
+      <Numbers text={'Neutra'} num={score.neutral} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
